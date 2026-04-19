147bc5d64c911ee2e3d497518ddc5d305c1297fa
2rafaadi90-stack <2rafaadi90@gmail.com>
2026-04-19 12:26:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ const music = [
   {
     title: "Kalau Bukan Kamu",
     file: "Kalau Bukan Kamu 22.mp3",
-    cover: "https://via.placeholder.com/300"
+    cover: "https://picsum.photos/300?1"
   },
   {
     title: "Rumah Yang Salah",
     file: "Rumah Yang Salah.mp3",
-    cover: "https://via.placeholder.com/300"
+    cover: "https://picsum.photos/300?2"
   }
 ];
 
 const list = document.getElementById("musicList");
+const player = document.getElementById("audioPlayer");
+const nowPlaying = document.getElementById("nowPlaying");
 
 music.forEach((m) => {
   list.innerHTML += `
-    <div class="card">
+    <div class="card" onclick="playMusic('${m.file}', '${m.title}')">
       <img src="${m.cover}">
       <h3>${m.title}</h3>
-      <audio controls>
-        <source src="${m.file}" type="audio/mp3">
-      </audio>
     </div>
   `;
 });
+
+function playMusic(file, title) {
+  player.src = file;
+  player.play();
+  nowPlaying.innerText = "Now Playing: " + title;
+}
