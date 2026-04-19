d897a2f852e7585173c8b9bbd59be72d024dace0
2rafaadi90-stack <2rafaadi90@gmail.com>
2026-04-19 14:21:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,12 @@
-let music = [];
-let currentIndex = 0;
-let isPlaying = false;
+const eq = document.getElementById("eq");
+const audio = document.getElementById("audioPlayer");
 
-const player = document.getElementById("audioPlayer");
-const list = document.getElementById("musicList");
-const nowPlaying = document.getElementById("nowPlaying");
-const playBtn = document.getElementById("playBtn");
-const progress = document.getElementById("progress");
-const search = document.getElementById("search");
-
-// ambil data lagu
-fetch("music.json")
-.then(res=>res.json())
-.then(data=>{
-  music = data;
-  renderMusic(music);
-});
-
-// tampilkan lagu
-function renderMusic(data){
-  list.innerHTML="";
-  data.forEach((m,i)=>{
-    list.innerHTML+=`
-    <div class="card">
-      <h3>${m.title}</h3>
-      <div class="play-btn" onclick="playMusic(${i})">▶</div>
-    </div>`;
-  });
-}
-
-// play
-function playMusic(i){
-  currentIndex=i;
-  player.src=music[i].file;
-  player.play();
-  isPlaying=true;
-  playBtn.innerText="⏸";
-  nowPlaying.innerText=music[i].title;
-}
-
-// toggle play
-function togglePlay(){
-  if(isPlaying){
-    player.pause();
-    playBtn.innerText="▶";
+function togglePlay() {
+  if (audio.paused) {
+    audio.play();
+    eq.style.opacity = 1;
   } else {
-    player.play();
-    playBtn.innerText="⏸";
+    audio.pause();
+    eq.style.opacity = 0.3;
   }
-  isPlaying=!isPlaying;
 }
-
-// next prev
-function next(){
-  currentIndex++;
-  if(currentIndex>=music.length) currentIndex=0;
-  playMusic(currentIndex);
-}
-
-function prev(){
-  currentIndex--;
-  if(currentIndex<0) currentIndex=music.length-1;
-  playMusic(currentIndex);
-}
-
-// progress
-player.ontimeupdate=()=>{
-  const percent=(player.currentTime/player.duration)*100;
-  progress.style.width=percent+"%";
-};
-
-// seek
-function seek(e){
-  const width=e.currentTarget.clientWidth;
-  const click=e.offsetX;
-  player.currentTime=(click/width)*player.duration;
-}
-
-// search
-search.oninput=()=>{
-  const k=search.value.toLowerCase();
-  renderMusic(music.filter(m=>m.title.toLowerCase().includes(k)));
-};
